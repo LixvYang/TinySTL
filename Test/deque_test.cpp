43f3e3d29eafc0665b78fe25424fe80d2ba8e4a0
lixin <2690688423@qq.com>
2022-08-22 15:43:14 +0800

    update

--- Test/deque_test.cpp
+++ Test/deque_test.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 #include "../TinySTL/deque.h"
 
-void printDeque(tinystl::deque<int> d1)
+template <class T>
+void printDeque(tinystl::deque<T> d1)
 {
+  for (tinystl::deque<int>::iterator it = d1.begin(); it != d1.end(); it++)
+  {
+    std::cout << *it << " ";
+  }
+}
+
+int main()
+{
+  tinystl::deque<int32_t> d1;
+  for (int i = 0; i < 10; i++)
+  {
+    d1.push_back(i);
+  }
+  d1.pop_back();
+  tinystl::deque<int>::iterator it = d1.begin();
+  it++;
+  std::cout << ++*it << std::endl;
+  *it = 10;
+  d1.pop_front();
+  d1.erase(it);
+  std::cout << d1.front() << std::endl;
   for (tinystl::deque<int>::const_iterator it = d1.begin(); it != d1.end(); it++)
   {
     std::cout << *it << " ";
   }
   std::cout << std::endl;
-}
-
-int main()
-{
-  tinystl::deque<int> d1;
-  for (int i = 0; i < 10; i++)
-  {
-    d1.push_back(i);
-  }
-  printDeque(d1);
+  // printDeque<int>(d1);
   return 0;
 }
